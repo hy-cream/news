15f2c3db1ca55a175492e4b0d69400c1c371c516
hy-cream <hylulalie@163.com>
2017-03-12 23:07:14 +0800

    Bean类
    json解析成bean类对象

---
+++ Flipped/app/src/main/java/com/example/huyu/bean/BaseBean.java
@@ -0,0 +1,15 @@
+package com.example.huyu.bean;
+
+import java.util.List;
+
+/**
+ * Created by huyu on 2017/3/11.
+ */
+
+public class BaseBean {
+
+    public boolean getError(){
+        return true;
+    }
+
+}
