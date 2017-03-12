4a6b6589d4689a180cb4f16f19593695fa6fc76b
hy-cream <hylulalie@163.com>
2017-03-12 23:06:06 +0800

    retrofit的借借口
    retrofit接口的定义，想尝试用泛型来代表不同的bean类，但这个父类设置待完善

---
+++ Flipped/app/src/main/java/com/example/huyu/http/IGanHuoService.java
@@ -0,0 +1,30 @@
+package com.example.huyu.http;
+
+import com.example.huyu.bean.AndroidBean;
+import com.example.huyu.bean.BaseBean;
+import com.example.huyu.bean.IOSBean;
+import com.example.huyu.bean.MeiZhiBean;
+import com.example.huyu.flipped.BeanFactory;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by huyu on 2017/3/10.
+ */
+
+public interface IGanHuoService {
+
+    @GET("data/Android/{count}/{page}")
+    Call<AndroidBean> getAndroidList(@Path("count") int count,
+                                    @Path("page") int page);
+    @GET("data/iOS/{count}/{page}")
+    Call<IOSBean> getIosList(@Path("count") int count,
+                             @Path("page") int page);
+    @GET("data/福利/{count}/{page}")
+    Call<MeiZhiBean> getMeizhiList(@Path("count") int count,
+                                   @Path("page") int page);
+}
