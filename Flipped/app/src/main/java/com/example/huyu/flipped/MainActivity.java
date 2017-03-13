3c4840f3795803bb19a0c037e38fad548f593537
hy-cream <hylulalie@163.com>
2017-03-13 23:13:22 +0800

    MainActivity优化
    hactivity与Fragment之间的通信

--- Flipped/app/src/main/java/com/example/huyu/flipped/MainActivity.java
+++ Flipped/app/src/main/java/com/example/huyu/flipped/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.huyu.flipped;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.example.huyu.adapter.MainVpAdapter;
 import com.example.huyu.bean.AndroidBean;
 import com.example.huyu.fragment.AndroidFragment;
 import com.example.huyu.fragment.IosFragment;
 import com.example.huyu.fragment.MeizhiFragment;
-import com.example.huyu.fragment.VideoFragment;
+import com.example.huyu.fragment.XiaFragment;
 import com.example.huyu.http.HttpManager;
 
 import java.util.ArrayList;
@@ -24,10 +27,60 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout mTl;
     private ViewPager mVp;
     private List<Fragment> mFragmentList;
-    private  String[] mTitles={"ANDROID","IOS","休闲视频","妹子图"};
+    private  String[] mTitles={"ANDROID","IOS","瞎推荐","妹子图"};
     private int[] mImgs={R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher};
     private MainVpAdapter mMainVpAdapter;
-    private TabLayout.Tab androidTab,iosTab,videoTab,meizhiTab;
+    private TabLayout.Tab androidTab,iosTab,xiaTab,meizhiTab;
+    private static final int ANDROID_CODE=0x001;
+    private static final int IOS_CODE=0x002;
+    private static final int MEIZHI_CODE=0x003;
+    private static final int XIA_CODE=0x004;
+
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case ANDROID_CODE:
+                    //由于他在viewPager里面动态生成，所以这里取出来时一个空对象
+//                    AndroidFragment androidFragment= (AndroidFragment) getSupportFragmentManager().findFragmentById(R.id.android);
+//                    if (androidFragment!=null){
+//                        androidFragment.initView(msg.obj);
+//                    }
+                    //基于FragmentPagerAdapter的实现是有效的,数字表示在viewPager中位置
+                    AndroidFragment androidf= (AndroidFragment) getSupportFragmentManager().findFragmentByTag("android:switcher:"+R.id.main_vp+":0");
+                    if (androidf!=null){
+                        androidf.initView(msg.obj);
+                    }
+                    break;
+
+                case IOS_CODE:
+                    IosFragment iosf= (IosFragment) getSupportFragmentManager().findFragmentByTag("android:switcher:"+R.id.main_vp+":1");
+                    if (iosf!=null){
+                        iosf.initView(msg.obj);
+                    }
+                    break;
+
+                case MEIZHI_CODE:
+                    MeizhiFragment meizhif= (MeizhiFragment) getSupportFragmentManager().findFragmentByTag("android:switcher:"+R.id.main_vp+":3");
+                    if (meizhif!=null){
+                        meizhif.initView(msg.obj);
+                    }
+                    else {
+                        Log.i("meizhitu------------","fragment is null");
+                    }
+                    break;
+
+                case XIA_CODE:
+                    XiaFragment xiaf= (XiaFragment) getSupportFragmentManager().findFragmentByTag("android:switcher:"+R.id.main_vp+":2");
+                    if (xiaf!=null){
+                        xiaf.initView(msg.obj);
+                    }
+                    break;
+
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +90,16 @@ public class MainActivity extends AppCompatActivity {
         initView();
         initData();
 
-        //new HttpManager().retrofitHttpGet("Android",20,1);
-
     }
 
     private void initData() {
-        AndroidFragment android=new AndroidFragment();
-        IosFragment ios=new IosFragment();
-        VideoFragment video=new VideoFragment();
-        MeizhiFragment meizhi=new MeizhiFragment();
+        AndroidFragment android=new AndroidFragment(handler);
+        IosFragment ios=new IosFragment(handler);
+        XiaFragment xia=new XiaFragment(handler);
+        MeizhiFragment meizhi=new MeizhiFragment(handler);
         mFragmentList.add(android);
         mFragmentList.add(ios);
-        mFragmentList.add(video);
+        mFragmentList.add(xia);
         mFragmentList.add(meizhi);
 
 
@@ -59,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         //
         androidTab = mTl.getTabAt(0).setIcon(mImgs[0]);
         iosTab= mTl.getTabAt(1).setIcon(mImgs[1]);
-        videoTab = mTl.getTabAt(2).setIcon(mImgs[2]);
+        xiaTab = mTl.getTabAt(2).setIcon(mImgs[2]);
         meizhiTab= mTl.getTabAt(3).setIcon(mImgs[3]);
 
 
